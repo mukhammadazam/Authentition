736d8b8196ffa4f86fcc17ffe5a6fc03dbb02ac2
mukhammadazam <rizayevmuhammadazam9@gmail.com>
2023-06-18 16:58:21 +0500

    ok

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,39 +1,38 @@
 import { useNavigate } from "react-router-dom";
 import axios from "../service/axios.js";
 import React, { useEffect, useState } from "react";
+import AuthServes from "../service/Function.jsx";
+import useToken from "../hooks/useToken.js";
 const Profile = () => {
   const [mydata, setData] = useState(null);
   const navigate = useNavigate();
-  const token = useState(localStorage.getItem("token") || null);
+  const [token, setToken] = useToken("token");
+  console.log(token);
   useEffect(() => {
-    axios
-      .get("/auth/user", {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((res) => {
-        setData(res.data.data.name);
-        console.log(res.data);
-      })
-      .catch((error) => {
-        // navigate("/");
-
-        console.error(error);
+    if (token) {
+      AuthServes.getAuthedUser(token).then((data) => {
+        console.log(data);
+        if (data.status === 200) {
+          localStorage.getItem('token');
+          console.log(data.data.data);
+          setData(data.data.data);
+        }
       });
-  }, [token]);
+    }
+  }, []);
   if (!token) return navigate("/");
 
   const logout = () => {
-    localStorage.removeItem("token");
+    setToken(null);
     navigate("/");
   };
-console.log(mydata);
+
   return (
     <div>
       <button onClick={logout}>Logout</button>
-      <h1>{mydata}</h1>
+      <h1>{mydata?.name}</h1>
+      <h2>{mydata?.email}</h2>
+      <h3>{mydata?.role_name}</h3>
     </div>
   );
 };
